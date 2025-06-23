34607ab95e8153693379f8a18d57779a2d99da2a
khariton90 <chinatruckspb@gmail.com>
2025-06-23 21:40:30 +0300

    feat(base): changing folder structure

--- server/dist/main.js
+++ server/dist/main.js
@@ -21,41 +21,6 @@ exports.BITCOIN_LAST_MONTH_HISTORY_API = `${API_PATH}/histoday?fsym=BTC&tsym=USD
 
 /***/ }),
 
-/***/ "./src/app.controller.ts":
-/*!*******************************!*\
-  !*** ./src/app.controller.ts ***!
-  \*******************************/
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var _a;
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.AppController = void 0;
-const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
-const app_service_1 = __webpack_require__(/*! ./app.service */ "./src/app.service.ts");
-let AppController = class AppController {
-    constructor(appService) {
-        this.appService = appService;
-    }
-};
-exports.AppController = AppController;
-exports.AppController = AppController = __decorate([
-    (0, common_1.Controller)(),
-    __metadata("design:paramtypes", [typeof (_a = typeof app_service_1.AppService !== "undefined" && app_service_1.AppService) === "function" ? _a : Object])
-], AppController);
-
-
-/***/ }),
-
 /***/ "./src/app.module.ts":
 /*!***************************!*\
   !*** ./src/app.module.ts ***!
@@ -72,8 +37,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.AppModule = void 0;
 const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
-const app_controller_1 = __webpack_require__(/*! ./app.controller */ "./src/app.controller.ts");
-const app_service_1 = __webpack_require__(/*! ./app.service */ "./src/app.service.ts");
 const bitcoin_price_module_1 = __webpack_require__(/*! ./bitcoin-price/bitcoin-price.module */ "./src/bitcoin-price/bitcoin-price.module.ts");
 const exchange_api_module_1 = __webpack_require__(/*! ./exchange-api/exchange-api.module */ "./src/exchange-api/exchange-api.module.ts");
 const schedule_module_1 = __webpack_require__(/*! ./schedule/schedule.module */ "./src/schedule/schedule.module.ts");
@@ -99,41 +62,10 @@ exports.AppModule = AppModule = __decorate([
             exchange_api_module_1.ExchangeApiModule,
             schedule_module_1.MainScheduleModule,
         ],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        controllers: [],
+        providers: [],
     })
 ], AppModule);
-
-
-/***/ }),
-
-/***/ "./src/app.service.ts":
-/*!****************************!*\
-  !*** ./src/app.service.ts ***!
-  \****************************/
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.AppService = void 0;
-const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
-let AppService = class AppService {
-    constructor() { }
-};
-exports.AppService = AppService;
-exports.AppService = AppService = __decorate([
-    (0, common_1.Injectable)(),
-    __metadata("design:paramtypes", [])
-], AppService);
 
 
 /***/ }),
@@ -148,7 +80,7 @@ exports.AppService = AppService = __decorate([
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.BAD_REQUEST_MESSAGE = exports.DEFAULT_LIMIT = void 0;
 exports.DEFAULT_LIMIT = 24;
-exports.BAD_REQUEST_MESSAGE = 'Невозможно обработать запрос';
+exports.BAD_REQUEST_MESSAGE = 'Request cannot be processed';
 
 
 /***/ }),
@@ -926,7 +858,7 @@ async function bootstrap() {
     app.useGlobalPipes(new common_1.ValidationPipe({
         transform: true,
     }));
-    const port = process.env.PORT ?? 3000;
+    const port = process.env.PORT ?? 3001;
     await app.listen(port);
     common_1.Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
